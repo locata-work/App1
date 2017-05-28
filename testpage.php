396df68faf3a3e875a1a2898d22b16880ae3e6c4
LOCA\LOCATA <tuanh0501@gmail.com>
2017-05-28 20:48:42 +0700

    # Update test page

--- testpage.php
+++ testpage.php
@@ -1,8 +1,14 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>This is test page</h1>
-
-</body>
-</html>
+<?php
+	if (isset($_GET["a"])) {
+		if($_GET['a']) == '1')
+		{
+		  echo <h1>This is A</h1>
+		}
+		else{
+			echo <h1>This is NOT A</h1>
+		}
+		
+	} else {
+		echo "Không có dữ liệu trả về";
+	}
+?>
