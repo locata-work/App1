450f845e5aace019d455bf037497c6282590b308
LOCA\LOCATA <tuanh0501@gmail.com>
2017-05-28 22:23:57 +0700

    no message

--- TestData/Connection.php
+++ TestData/Connection.php
@@ -1,9 +1,10 @@
 <?php
-$server_username = "locata"; // điền username đăng nhập mysql
-$server_password = "locatapass"; // điền password đăng nhập mysql
-$server_host = "localhost"; // điền tện host
-$database = 'test'; // tên database
+$server_username = "sql12176978"; // điền username đăng nhập mysql
+$server_password = "a8daTQXWTX"; // điền password đăng nhập mysql
+$server_host = "sql12.freemysqlhosting.net"; // điền tện host
+$database = 'sql12176978'; // tên database
+$server_port = '3306';
 
 // tạo biến kết nối tới database
-$conn = mysqli_connect($server_host, $server_username, $server_password, $database) or die("không thể kết nối tới database");
+$conn = mysqli_connect($server_host, $server_username, $server_password, $database,$server_port) or die("không thể kết nối tới database");
 ?>
