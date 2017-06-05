8c50711f05a6d3386059f38747a7955a6845b059
LOCA\LOCATA <tuanh0501@gmail.com>
2017-06-06 00:59:32 +0700

    # Update server

--- TestData/Connection.php
+++ TestData/Connection.php
@@ -1,10 +1,9 @@
 <?php
-$server_username = "sql12176978"; // điền username đăng nhập mysql
-$server_password = "a8daTQXWTX"; // điền password đăng nhập mysql
-$server_host = "sql12.freemysqlhosting.net"; // điền tện host
-$database = 'sql12176978'; // tên database
-$server_port = '3306';
-
+$server_username = "b9_20194776"; // điền username đăng nhập mysql
+$server_password = "nguyentuananh0501"; // điền password đăng nhập mysql
+$server_host = "sql211.byethost9.com";// điền tện host
+$database = "b9_20194776_locata"; // tên database
+$port = "3360";
 // tạo biến kết nối tới database
-$conn = mysqli_connect($server_host, $server_username, $server_password, $database,$server_port) or die("không thể kết nối tới database");
+$conn = mysqli_connect($server_host,$server_username,$server_password,$database,$port) or die("không thể kết nối tới database");
 ?>
