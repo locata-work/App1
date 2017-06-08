07481069fb7958ee327ee6f8abeb4511190067f6
LOCATA <tuanh0501@gmail.com>
2017-06-08 12:44:34 +0700

    Create Connection.php

--- TestData/Connection.php
+++ TestData/Connection.php
@@ -1,8 +1,8 @@
 <?php
-$server_username = "b9_20194776"; // điền username đăng nhập mysql
+$server_username = "locata_username"; // điền username đăng nhập mysql
 $server_password = "nguyentuananh0501"; // điền password đăng nhập mysql
-$server_host = "sql211.byethost9.com";// điền tện host
-$database = "b9_20194776_locata"; // tên database
+$server_host = "64.62.211.131";// điền tện host
+$database = "locata_database"; // tên database
 $port = "3360";
 // tạo biến kết nối tới database
 $conn = mysqli_connect($server_host,$server_username,$server_password,$database,$port) or die("không thể kết nối tới database");
